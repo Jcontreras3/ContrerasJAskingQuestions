3ec4203011256a8e5bcd2d5951ae56eca3fb6eef
Jovann Contreras <jcontreras@codestack.co>
2022-10-18 15:30:49 -0700

    ran into problems

--- Program.cs
+++ Program.cs
@@ -5,44 +5,68 @@
 
 
 Console.Clear();
+string playAgain = "yes";
 int time = 0;
 bool wokeUpTime = false;
-Console.WriteLine("Welcome to Challenge 3 Asking Questions");
-Console.WriteLine("What is your name?");
-string userName = Console.ReadLine();
-while (string.IsNullOrEmpty(userName))
+while (playAgain == "yes")
 {
 
-    Console.WriteLine("No name was entered");
-}
 
-Console.WriteLine($"Name: {userName}");
-
-
-while (!wokeUpTime)
-{
-    Console.WriteLine("What time did you wake up today?");
-    wokeUpTime = Int32.TryParse(Console.ReadLine(), out time);
-    if (wokeUpTime)
+    Console.WriteLine("Welcome to Challenge 3 Asking Questions");
+    Console.WriteLine("What is your name?");
+    string userName = Console.ReadLine();
+    while (string.IsNullOrEmpty(userName))
     {
-        Console.WriteLine($"Time: {time}");
-    }
-    else
-    {
-        Console.WriteLine("Not a specific time");
+
+        Console.WriteLine("No name was entered");
     }
 
-    Console.WriteLine("Was it Am or Pm?");
-    string amOrpm = "am";
+    Console.WriteLine($"Name: {userName}");
 
-    amOrpm = Console.ReadLine().ToLower();
-    if (amOrpm == "am")
+
+    while (!wokeUpTime)
     {
-        Console.WriteLine($"Hello {userName} you woke up at {time}am");
+        Console.WriteLine("What time did you wake up today?");
+        wokeUpTime = Int32.TryParse(Console.ReadLine(), out time);
+        if (wokeUpTime)
+        {
+            Console.WriteLine($"Time: {time}");
+        }
+        else
+        {
+            Console.WriteLine("Not a specific time");
+        }
+
+
     }
 
-    else if (amOrpm == "pm")
+   
+
+    Console.WriteLine("Would you like to try again? type in Yes to retry or No to quit");
+
+    string playAgainInput = "";
+
+    while (playAgainInput != "yes" || playAgainInput != "no")
     {
-        Console.WriteLine($"Hello {userName} you woke up at {time}pm");
+        playAgainInput = Console.ReadLine().ToLower();
+        if (playAgainInput == playAgain)
+        {
+            break;
+        }
+        else if (playAgainInput == "no")
+        {
+            Console.WriteLine("Thanks for playing!");
+            playAgain = playAgainInput;
+            break;
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+            playAgainInput = "invalid";
+        }
+
+
     }
+
 }
