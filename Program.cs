c30ece098ff87042464c0b34e211d9b9e88bcb78
Jovann Contreras <jcontreras@codestack.co>
2022-10-20 10:32:42 -0700

    cleared comments

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿//Jovann Contreras
-//Date 10/18/2022
-//Desc: I am asking the user some questions such as what is their name and what time they woke up today.
-
-
+﻿
 
 Console.Clear();
 string playAgain = "yes";
